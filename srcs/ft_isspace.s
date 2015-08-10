25bbc269a419ec5348e227b3e0c746118189835e
Julien Balestra <julien.balestra@gmail.com>
2015-08-10 16:09:35 +0200

    Is something more

---
+++ srcs/ft_isspace.s
@@ -0,0 +1,19 @@
+section .text
+	global _ft_isspace
+
+ft_isspace:
+	cmp		rdi, 32
+	je		true
+	cmp		rdi, 9
+	je		true
+	cmp		rdi, 13
+	je		true
+	jmp		false
+
+true:
+	mov		rax, 1
+	ret
+
+false:
+	mov		rax, 0
+	ret
